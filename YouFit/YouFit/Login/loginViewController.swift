e3ed5ee5ccec01939a7e3f22440676a7550f9e2f
tapadyuti1991 <tapadyuti@gmail.com>
2018-04-26 15:43:26 -0400

    "SomeChanges in Login Page Cage Still"

--- YouFit/YouFit/Login/loginViewController.swift
+++ YouFit/YouFit/Login/loginViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class loginViewController: UIViewController {
 
+   
+    
+    @IBOutlet weak var grad: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        animateBackground()
         
     }
     
@@ -25,6 +29,15 @@ class loginViewController: UIViewController {
     @IBAction func backButton(_ sender: UIBarButtonItem) {
         dismiss(animated: true, completion: nil)
         
+    }
+    
+    
+    func animateBackground(){
+        print("Inside animatiton")
+        UIView.animate(withDuration: 15, delay: 0, options: [.autoreverse,.curveLinear,.repeat], animations: {
+            let x = -(self.grad.frame.width - self.view.frame.width * 2)
+            self.grad.transform = CGAffineTransform(translationX: x, y: 0)
+        })
     }
     
 
